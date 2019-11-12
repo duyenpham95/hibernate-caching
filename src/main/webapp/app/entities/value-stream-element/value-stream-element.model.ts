21269896860e98fcd2d177f626de3829d9cdbf83
Duyen <fantasticJilove@gmail.com>
2019-11-13 00:01:00 +0700

    - Too many configuration - build a simpler with spring initializr

--- src/main/webapp/app/entities/value-stream-element/value-stream-element.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity } from './../../shared';
-
-export class ValueStreamElement implements BaseEntity {
-    constructor(
-        public id?: number,
-        public elementName?: string,
-    ) {
-    }
-}
